8dd726c61e3617ed72c570330a468dd63d0c9535
Cholay <michael.cholay@gmail.com>
2020-07-11 19:54:09 +0200

    update of port variable for heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var jsonParser = bodyParser.json()
 app.use(jsonParser);
 require('dotenv').config()
 
-const nodePort = process.env.PORT_NODE || PORT;
+const nodePort = process.env.PORT_NODE || PORT_NODE;
 
 
 //les routes en /html/... seront gérées par express
