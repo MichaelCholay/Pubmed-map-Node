210d66a8a08ca019d5be3c64af77f398ad010a06
Cholay <michael.cholay@gmail.com>
2020-07-12 18:12:40 +0200

    update scheduled-job.js file

--- scheduled-job.js
+++ scheduled-job.js
@@ -1,7 +1,4 @@
 const articlesApiRoutes = require('./articles-api-routes');
 
-// articlesApiRoutes = require('./articles-api-routes')
-
 console.log("Scheduler running ...");
-articlesApiRoutes.find_Pmid_bySearch_with_terms()
-console.log("Scheduler end !!!");
+articlesApiRoutes.find_Pmid_bySearch_with_terms()   
